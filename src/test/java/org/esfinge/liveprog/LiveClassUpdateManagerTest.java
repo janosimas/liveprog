52f88b2cc48149e8b6baac8dabd2dc9328c682b4
Jano Simas <janosimas@gmail.com>
2018-08-31 16:38:18 -0300

    test class update with no observers

--- src/test/java/org/esfinge/liveprog/LiveClassUpdateManagerTest.java
+++ src/test/java/org/esfinge/liveprog/LiveClassUpdateManagerTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.never;
 
 import org.esfinge.liveprog.instrumentation.ClassInfo;
 import org.esfinge.liveprog.instrumentation.inspector.InspectorHelper;
@@ -21,10 +22,24 @@ public class LiveClassUpdateManagerTest {
     LiveClassUpdateManager updateManager = new LiveClassUpdateManager();
     ClassInfo bInfo = (new InspectorHelper()).inspect(ClassB.class);
 
+    // update with no observer registered
+    updateManager.classFileUpdated(bInfo);
+
+    verify(observerMock, never()).liveClassUpdated(any(), any());
+    reset(observerMock);
+
+    // update with observer registered
     updateManager.addObserver(observerMock);
     updateManager.classFileUpdated(bInfo);
 
     verify(observerMock, times(1)).liveClassUpdated(eq(ClassB.class.getName()), any());
+    reset(observerMock);
+
+    // update with no observer registered
+    updateManager.removeObserver(observerMock);
+    updateManager.classFileUpdated(bInfo);
+
+    verify(observerMock, never()).liveClassUpdated(any(), any());
     reset(observerMock);
   }
 }
